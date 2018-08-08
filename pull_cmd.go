0278480b9c2bb790d1142c644e09e5884467043e
siddontang <siddontang@gmail.com>
2018-08-08 08:52:28 +0800

    add pull command

---
+++ pull_cmd.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+const TimeFormat string = "2006-01-02 15:04:05"
+
+var (
+	pullsState     string
+	pullsLimit     int
+	pullsSinceTime string
+	pullsOffsetDur string
+)
+
+func newPullsCommand() *cobra.Command {
+	m := &cobra.Command{
+		Use:   "pulls",
+		Short: "Github CLI for listing pulls",
+		Args:  cobra.MinimumNArgs(0),
+		Run:   runPullsCommandFunc,
+	}
+	m.Flags().StringVar(&pullsState, "state", "open", "PR state: open or closed")
+	m.Flags().IntVar(&pullsLimit, "limit", 100, "Maximum pull limit for a repository")
+	m.Flags().StringVar(&pullsSinceTime, "since", "", fmt.Sprintf("Pull Since Time, format is %s", TimeFormat))
+	m.Flags().StringVar(&pullsOffsetDur, "offset", "-336h", "The offset of since time")
+	return m
+}
+
+func runPullsCommandFunc(cmd *cobra.Command, args []string) {
+	opts := NewPullOptions()
+	opts.State = pullsState
+	opts.Limit = pullsLimit
+
+	if len(pullsSinceTime) > 0 {
+		end, err := time.Parse(TimeFormat, pullsSinceTime)
+		perror(err)
+		opts.End = end
+	}
+
+	d, err := time.ParseDuration(pullsOffsetDur)
+	perror(err)
+	opts.Start = opts.End.Add(d)
+	if opts.Start.After(opts.End) {
+		opts.Start, opts.End = opts.End, opts.Start
+	}
+
+	m, err := globalClient.ListPulls(globalCtx, opts)
+	perror(err)
+
+	for repo, pulls := range m {
+		fmt.Println(repo)
+		for _, pull := range pulls {
+			fmt.Printf("%s %s %s\n", pull.GetUpdatedAt().Format(TimeFormat), pull.GetHTMLURL(), pull.GetTitle())
+		}
+	}
+}
+
+var (
+	pullCommentLimit int
+)
+
+func newPullCommand() *cobra.Command {
+	m := &cobra.Command{
+		Use:   "pull [owner] [repo] [id]",
+		Short: "Github CLI for getting one pull",
+		Args:  cobra.MinimumNArgs(3),
+		Run:   runPullCommandFunc,
+	}
+
+	m.Flags().IntVar(&pullCommentLimit, "comments-limit", 3, "Comments limit")
+	return m
+}
+
+func runPullCommandFunc(cmd *cobra.Command, args []string) {
+	owner := args[0]
+	repo := args[1]
+	id, err := strconv.Atoi(args[2])
+	perror(err)
+
+	pull, err := globalClient.GetPull(globalCtx, owner, repo, id)
+	perror(err)
+
+	comments, err := globalClient.ListPullComments(globalCtx, owner, repo, pull.GetComments())
+	perror(err)
+
+	fmt.Printf("Title: %s\n", pull.GetTitle())
+	fmt.Printf("Created at %s\n", pull.GetCreatedAt().Format(TimeFormat))
+	fmt.Printf("Message:\n %s\n", pull.GetBody())
+	if len(comments) > pullCommentLimit {
+		comments = comments[0:pullCommentLimit]
+	}
+	for _, comment := range comments {
+		fmt.Printf("Comment:\n %s\n", comment.GetBody())
+	}
+}
