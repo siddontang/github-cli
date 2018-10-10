33fd3844ee9b34222d7070237cb5d3d95996303e
siddontang <siddontang@gmail.com>
2018-10-10 14:11:00 +0800

    simpify repo configuration

--- cfg.go
+++ cfg.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
 
 // Repository is the config for the repository at Github
 type Repository struct {
-	Owner string `toml:"owner"`
-	Name  string `toml:"name"`
+	Owner string
+	Name  string
 }
 
 func (r Repository) String() string {
 	return fmt.Sprintf("%s/%s", r.Owner, r.Name)
+}
+
+// UnmarshalText implementes TOML interface.
+func (r *Repository) UnmarshalText(text []byte) error {
+	seps := strings.Split(string(text), "/")
+	if len(seps) != 2 {
+		return fmt.Errorf("repository must be owner/name format")
+	}
+
+	r.Owner, r.Name = seps[0], seps[1]
+	return nil
 }
 
 // Slack configuration
